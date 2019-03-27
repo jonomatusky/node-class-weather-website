ca28eeaf741bbdfbe56afab3ae26f19c1530662a
Jono Matusky <jono.matusky@gmail.com>
2019-03-26 20:35:18 -0400

    Add highs and lows

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.')
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability*100 + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability*100 + '% chance of rain. The expected high is ' + body.daily.data[0].temperatureMax + ' and the expected low is ' + body.daily.data[0].temperatureMin + '.')
         }
     })
 }
